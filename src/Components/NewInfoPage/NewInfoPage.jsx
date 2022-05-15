0d1ceb34aeece4588eed4ab11e286fdfccd739e2
Mihail <mihailgerasimov86@gmail.com>
2022-05-15 23:24:03 +0400

    pravki

--- src/Components/NewInfoPage/NewInfoPage.jsx
+++ src/Components/NewInfoPage/NewInfoPage.jsx
@@ -8,7 +8,7 @@ import lupa from "./lupa.png";
 function News() {
   const [news, setNews] = useState([]);
   const [searchInput, setSearchInput] = useState("");
-  const [selectChanged, setSelectChanged] = useState("");
+  const [selectChanged, setSelectChanged] = useState("title");
   const [modeEntity, setModeEntity] = useState("");
 
   const [currentPage, setCurrentPage] = useState(1);
@@ -95,6 +95,14 @@ function News() {
     let sortColumn = sortDesc ? "" : "sort";
     setModeEntity(sortColumn);
   };
+
+  const setupFind = () => {
+    setNews([]);
+    setFetching(true);
+    setCurrentPage(1);
+    setModeEntity("find");
+  };
+
   return (
     <div className="main-container">
       <div className="finderStyle">
@@ -107,32 +115,7 @@ function News() {
             onChange={(event) => setSearchInput(event.target.value)}
           />
 
-          <button
-            className="buttonFind inputStyle"
-            onClick={() => {
-              setNews([]);
-              setFetching(true);
-              setModeEntity("find");
-            }}
-          >
-            {/* <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g opacity="0.5">
-                <path
-                  d="M10 18C11.775 17.9996 13.4988 17.4054 14.897 16.312L19.293 20.708L20.707 19.294L16.311 14.898C17.405 13.4997 17.9996 11.7754 18 10C18 5.589 14.411 2 10 2C5.589 2 2 5.589 2 10C2 14.411 5.589 18 10 18ZM10 4C13.309 4 16 6.691 16 10C16 13.309 13.309 16 10 16C6.691 16 4 13.309 4 10C4 6.691 6.691 4 10 4Z"
-                  fill="#392B4B"
-                />
-                <path
-                  d="M11.4118 8.58603C11.7908 8.96603 11.9998 9.46802 11.9998 10H13.9998C14.0007 9.47445 13.8974 8.95392 13.6959 8.46851C13.4944 7.9831 13.1987 7.54245 12.8258 7.17202C11.3118 5.66002 8.68683 5.66002 7.17383 7.17202L8.58583 8.58803C9.34583 7.83003 10.6558 7.83203 11.4118 8.58603Z"
-                  fill="#392B4B"
-                />
-              </g>
-            </svg> */}
+          <button className="buttonFind inputStyle" onClick={setupFind}>
             <img src={lupa} alt="" className="lupaPA" />
           </button>
         </div>
@@ -142,7 +125,6 @@ function News() {
           onChange={(event) => setSelectChanged(event.target.value)}
           value={selectChanged}
         >
-          <option hidden>Поиск...</option>
           <option value="title">По заголовку</option>
           <option value="summary">По описанию</option>
         </select>
